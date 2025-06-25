e5720fae4c0eb77df972f5571ca1270a810f4bef
Stuart Foy <stu@rtfoy.co.uk>
2025-06-25 15:06:03 +0100

    mystery

---
+++ js/theme-init.js
@@ -0,0 +1,6 @@
+      // Check for a saved theme in localStorage and apply it, or default to the OS preference.
+      if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+      }
